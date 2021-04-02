4231b142ad00e0f545169315cd381602acae1c3a
gennady-bars <54933269+gennady-bars@users.noreply.github.com>
2021-04-02 10:19:25 +0300

    add build folder

---
+++ precache-manifest.9050b2e5f3b65ce022638f3bd28c0940.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f1469bb765e3ede91eae732fe80a28",
+    "url": "/vepay-admin/index.html"
+  },
+  {
+    "revision": "d5c58487dc710b5397d2",
+    "url": "/vepay-admin/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "4689b405545c71c6e91c",
+    "url": "/vepay-admin/static/css/main.8fa2eafe.chunk.css"
+  },
+  {
+    "revision": "d5c58487dc710b5397d2",
+    "url": "/vepay-admin/static/js/2.c86cff97.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/vepay-admin/static/js/2.c86cff97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4689b405545c71c6e91c",
+    "url": "/vepay-admin/static/js/main.e53ca2d9.chunk.js"
+  },
+  {
+    "revision": "562ed3a8660672ec0859",
+    "url": "/vepay-admin/static/js/runtime-main.69f79693.js"
+  },
+  {
+    "revision": "24b74a4bebc5d6ae662fcfbba901a2e7",
+    "url": "/vepay-admin/static/media/logo-white-text.24b74a4b.svg"
+  }
+]);
